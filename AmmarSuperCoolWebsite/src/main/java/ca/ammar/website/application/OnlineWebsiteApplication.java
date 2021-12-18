fcf1fd430b7471ef3271d44f5bbbf9238740f104
Waseemam <78282491+Waseemam@users.noreply.github.com>
2021-12-18 17:28:35 -0500

    Upload Maiin

---
+++ AmmarSuperCoolWebsite/src/main/java/ca/ammar/website/application/OnlineWebsiteApplication.java
@@ -0,0 +1,13 @@
+package ca.ammar.website.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineWebsiteApplication.class, args);
+    }
+
+}
